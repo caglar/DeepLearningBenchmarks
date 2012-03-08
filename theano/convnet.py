24c72ade15558f9236ee2c722d7c17fc9d5127a6
Caglar <ca9lar@gmail.com>
2012-03-08 18:11:05 -0500

    Bench reporter class added to the repository.

--- theano/convnet.py
+++ theano/convnet.py
@@ -24,10 +24,8 @@ def randn(*size):
 def randint(size, high):
     return asarray(random.randint(size=size, low=0, high=high), dtype='int32')
 
-
 def zeros(*size):
     return numpy.zeros(size, dtype=config.floatX)
-
 
 n_examples = 1000
 outputs = 10
@@ -133,7 +131,6 @@ def bench_ConvMed(batchsize):
             updates=[(p, p - lr * gp) for p, gp in zip(params, gparams)])
     eval_and_report(train, "ConvMed", [batchsize], N=120)
 
-
 def bench_ConvLarge(batchsize):
     data_x.set_value(randn(n_examples, 1, 256, 256))
     w0 = shared(rand(6, 1, 7, 7) * numpy.sqrt(6 / (25.)))
@@ -172,4 +169,3 @@ if __name__ == '__main__':
     bench_ConvMed(60) #gives error
     bench_ConvLarge(1)
     bench_ConvLarge(60)
-
